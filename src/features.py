32edc5a512b716f5f0fd41c124cfb01509951166
Lord Ennard <andrex9400@gmail.com>
2025-10-02 23:07:33 -0500

    Cambios en rama develop

--- src/features.py
+++ src/features.py
@@ -38,5 +38,5 @@ if __name__ == "__main__":
     ap.add_argument("--infile", default="data/raw/Telco-Customer-Churn.csv")
     ap.add_argument("--outfile", default="data/processed/telco_clean.csv")
     args = ap.parse_args()
-    print("Cambio de prueba")
+    print("HOLAAAAAA")
     main(args.infile, args.outfile)
--- src/train.py
+++ src/train.py
@@ -107,5 +107,5 @@ if __name__ == "__main__":
     ap.add_argument("--data", default="data/processed/telco_clean.csv")
     ap.add_argument("--out", default="artifacts")
     args = ap.parse_args()
-    print("Otro cambio de prueba")
+    print("HOLAAAAAA")
     main(args.data, args.out)
